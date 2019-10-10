89f7d2feecbd309a780c2ce61d0f6835217140d2
Dj <ritchey0713@gmail.com>
2019-10-09 20:23:17 -0400

    add dom manip

--- hangman.js
+++ hangman.js
@@ -4,6 +4,8 @@ const Hangman = function(word, remainingGuesses){
     this.guessedLetters = []
 }
 
+
+
 Hangman.prototype.getPuzzle = function(){
     let answer = ""
     this.word.forEach((letter) => {
@@ -31,17 +33,34 @@ Hangman.prototype.makeGuess = function(guess) {
 
 }
 
-
 const game1 = new Hangman("cat", 2)
 
+const gameDiv = document.querySelector("#game-wrapper")
 
-console.log(game1.getPuzzle())
-console.log(game1.remainingGuesses)
+const wordEl = document.createElement("div")
+wordEl.id = "word"
+wordEl.textContent = game1.getPuzzle()
+
+const guessesEl = document.createElement("p")
+guessesEl.id = "guesses-count"
+guessesEl.textContent = `remaining guesses: ${game1.remainingGuesses}`
+
+
+
+
+// console.log(game1.getPuzzle())
+// console.log(game1.remainingGuesses)
+
+
+gameDiv.appendChild(guessesEl).appendChild(wordEl)
+
 
 window.addEventListener("keydown", (e) => {
     if(e.code.includes("Key")){
         game1.makeGuess(e.key)
-        console.log(game1.getPuzzle())
+        guessesEl.textContent = `remaining guesses: ${game1.remainingGuesses}`
+        wordEl.textContent = game1.getPuzzle()
+        gameDiv.appendChild(guessesEl).appendChild(wordEl)
 
     }
 })
