18ed4bd9626a8c9e2d7879f6e90aec5c8cacee3f
Dj <ritchey0713@gmail.com>
2019-10-10 15:41:26 -0400

    add dom manip

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+game1.getStatus()
+game1.displayMessage()
 window.addEventListener("keydown", (e) => {
     if(e.code.includes("Key")){
-        game1.makeGuess(e.key)    
+        game1.makeGuess(e.key)   
     }
     wordEl.textContent = game1.getPuzzle()
     guessesEl.textContent = `remaining guesses: ${game1.remainingGuesses}`
-    gameDiv.appendChild(guessesEl).appendChild(wordEl)
+    gameDiv.appendChild(guessesEl).appendChild(wordEl).appendChild(statusEl)
 })
+
+gameDiv.appendChild(guessesEl).appendChild(wordEl).appendChild(statusEl)
