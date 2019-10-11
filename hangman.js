40ca14697a7bc6bab67a2dcef5fa03891e724a3e
Dj <ritchey0713@gmail.com>
2019-10-11 12:51:32 -0400

    add getter methods

--- hangman.js
+++ hangman.js
@@ -1,58 +1,86 @@
-const Hangman = function(word, remainingGuesses){
-    this.word = word.toLowerCase().split("") 
-    this.remainingGuesses = remainingGuesses
-    this.guessedLetters = []
-    this.status = "playing"
-}
+class HangmanClass {
+    constructor(word, remainingGuesses){
+        this.word = word.toLowerCase().split("") 
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
+        this.status = "playing"
+    }
 
+    gameStats = function () {
+        wordEl.textContent = this.getPuzzle
+        guessesEl.textContent = `remaining guesses: ${this.remainingGuesses}`
+    }
 
+    get getPuzzle() {
+        let answer = ""
+        this.word.forEach((letter) => {
+            if(this.guessedLetters.includes(letter) || letter === " "){
+                answer += letter
+            }else{
+                answer +="*"
+            }
+        })
+        return answer
+    }
 
-
-Hangman.prototype.getPuzzle = function(){
-    let answer = ""
-    this.word.forEach((letter) => {
-        if(this.guessedLetters.includes(letter) || letter === " "){
-            answer += letter
-        }else{
-            answer +="*"
+    makeGuess = function(guess) {
+        if(this.status === "playing"){
+            guess = guess.toLowerCase()
+            const uniqueLetter = !this.guessedLetters.includes(guess)
+            const badGuess = !this.word.includes(guess)
+    
+            if(uniqueLetter){
+                this.guessedLetters.push(guess)
+            }
+    
+            if (uniqueLetter && badGuess) {
+                this.remainingGuesses--
+            }
+            this.getStatus
+            this.gameStats()
+            this.displayMessage()
         }
-    })
-    return answer
-}
+    }
 
-const game1 = new Hangman("New egg", 5)
+    get getStatus() {
+        const finished = this.word.every((letter) => {
+            return this.guessedLetters.includes(letter)
+        }) 
+        if(this.remainingGuesses === 0){
+            this.status = "failed"
+        } else if (finished) {
+            this.status = "finished"
+        } else {
+            this.status = "playing"
+        }
+        return this.status
+    }
+
+    displayMessage = function () {
+        if(this.status === "finished") {
+            statusEl.textContent = `Great Job! Y ou guessed the word!`
+        } else if (this.status === 'failed') {
+            statusEl.textContent = `You Lost! The word was ${this.word.join("")}`
+        } else {
+            statusEl.textContent = "Now Playing!"
+        }
+    }
+    
+    
+}
 
 const gameDiv = document.querySelector("#game-wrapper")
 
 const wordEl = document.createElement("div")
 wordEl.id = "word"
-wordEl.textContent = game1.getPuzzle()
+
 
 const guessesEl = document.createElement("p")
 guessesEl.id = "guesses-count"
-guessesEl.textContent = `remaining guesses: ${game1.remainingGuesses}`
+
 
 const statusEl = document.createElement("p")
 statusEl.id = "status"
-
-
-Hangman.prototype.makeGuess = function(guess) {
-    if(this.status === "playing"){
-        guess = guess.toLowerCase()
-        const uniqueLetter = !this.guessedLetters.includes(guess)
-        const badGuess = !this.word.includes(guess)
-
-        if(uniqueLetter){
-            this.guessedLetters.push(guess)
-        }
-
-        if (uniqueLetter && badGuess) {
-            this.remainingGuesses--
-        }
-        this.getStatus()
-        this.displayMessage()
-    }
-}
 
 // Hangman.prototype.getStatus = function () {
 //     let counter = 0
@@ -74,30 +102,6 @@ Hangman.prototype.makeGuess = function(guess) {
 //         statusEl.textContent = `congrats!! You win!`
 //     }
 // }
-
-Hangman.prototype.getStatus = function () {
-    const finished = this.word.every((letter) => {
-        return this.guessedLetters.includes(letter)
-    }) 
-    if(this.remainingGuesses === 0){
-        this.status = "failed"
-    } else if (finished) {
-        this.status = "finished"
-    } else {
-        this.status = "playing"
-    }
-}
-
-Hangman.prototype.displayMessage = function () {
-    if(this.status === "finished") {
-        statusEl.textContent = `Great Job! Y ou guessed the word!`
-    } else if (this.status === 'failed') {
-        statusEl.textContent = `You Lost! The word was ${this.word.join("")}`
-    } else {
-        statusEl.textContent = "Now Playing!"
-    }
-}
-
 
 
 
