4d8681ce9f31da4ccb9dc441598611c349592fc5
Dj <ritchey0713@gmail.com>
2019-10-14 13:11:42 -0400

    add async call back

--- requests.js
+++ requests.js
@@ -1,19 +1,17 @@
-const getPuzzle = () => {
+const getPuzzle = (callback) => {
+
     const request = new XMLHttpRequest()
 
     request.addEventListener("readystatechange", (e) => {
         if (e.target.readyState === 4 && e.target.status === 200) {
             const data = JSON.parse(e.target.response)
-            console.log(data)
+            callback(null, data.puzzle)
         } else if (e.target.readyState === 4) {
-            console.log("an error happened??")
+            callback("an error happened", null)
         }
     })
 
     request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3")
 
     request.send()
-
-
-    return "Puzzle"
 }
