c6e1ec845720197c00050f8def1de58a6189d61c
Dj <ritchey0713@gmail.com>
2019-10-15 12:54:00 -0400

    refactor to fetch

--- requests.js
+++ requests.js
@@ -1,18 +1,13 @@
-const getPuzzle = (wordCount) => new Promise((res, rej) => {
-
-    const request = new XMLHttpRequest()
-
-    request.addEventListener("readystatechange", (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.response)
-            resolve(data.puzzle)
-        } else if (e.target.readyState === 4) {
-            reject("Error happened??")
+const getPuzzle = (wordCount) => {
+    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {}).then((resp) => {
+        if(resp.status === 200) {
+            return resp.json()
+        } else {
+            throw new Error("unable to fetch :[")
         }
+    }).then((data) => {
+        return data.puzzle
     })
+}
 
-    request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3")
-
-    request.send()
-})
 
