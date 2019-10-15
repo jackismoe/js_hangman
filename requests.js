7920b3d92781cda0ba1628e0f42afa67293b6493
Dj <ritchey0713@gmail.com>
2019-10-15 11:29:01 -0400

    refactor to promise

--- requests.js
+++ requests.js
@@ -1,17 +1,18 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount) => new Promise((res, rej) => {
 
     const request = new XMLHttpRequest()
 
     request.addEventListener("readystatechange", (e) => {
         if (e.target.readyState === 4 && e.target.status === 200) {
             const data = JSON.parse(e.target.response)
-            callback(null, data.puzzle)
+            resolve(data.puzzle)
         } else if (e.target.readyState === 4) {
-            callback("an error happened", null)
+            reject("Error happened??")
         }
     })
 
     request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3")
 
     request.send()
-}
+})
+
