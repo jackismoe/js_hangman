031ce171ba910839418c1070e417259e10318ac5
Dj <ritchey0713@gmail.com>
2019-10-16 13:37:35 -0400

    add async await

--- requests.js
+++ requests.js
@@ -1,11 +1,22 @@
-const getPuzzle = (wordCount) => {
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {}).then((resp) => {
-        if(resp.status === 200) {
-            return resp.json()
+// const getPuzzle = (wordCount) => {
+//     return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {}).then((resp) => {
+//         if(resp.status === 200) {
+//             return resp.json()
+//         } else {
+//             throw new Error("unable to fetch :[")
+//         }
+//     }).then((data) => {
+//         return data.puzzle
+//     })
+// }
+
+const getPuzzle = async(wordCount) => {
+    const resp = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {})
+        if (resp.status === 200) {
+            const data =  await resp.json()
+            return data.puzzle
         } else {
-            throw new Error("unable to fetch :[")
+            throw new Error("Unable to fetch")
         }
-    }).then((data) => {
-        return data.puzzle
-    })
-}
+    }
+    
